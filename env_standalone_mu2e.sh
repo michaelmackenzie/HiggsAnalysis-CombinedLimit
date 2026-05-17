47ad3fd06711907ee88321762dc3ee4ee60de1dc
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2026-05-17 11:10:57 -0500

    Update env

--- env_standalone_mu2e.sh
+++ env_standalone_mu2e.sh
@@ -5,9 +5,13 @@ source /cvmfs/mu2e.opensciencegrid.org/setupmu2e-art.sh
 
 GSL=/cvmfs/mu2e.opensciencegrid.org/artexternals/gsl/v2_7/Linux64bit+3.10-2.17/lib/
 BOOST=/cvmfs/mu2e.opensciencegrid.org/artexternals/boost/v1_82_0/Linux64bit+3.10-2.17-e28-prof/lib/
+TBB=/cvmfs/mu2e.opensciencegrid.org/artexternals/tbb/v2021_9_0/Linux64bit+3.10-2.17-e28/lib/
+export MUSE_WORK_DIR="${PWD}/../../.."
+export MUSE_BUILD_DIR=`ls -dt ${PWD}/../../../build/* | head -n 1`
 
 export PATH=${PATH}:${PWD}/build/bin:${PWD}/scripts:${PWD}/../../vdt/:
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/build/lib:${PWD}/../../vdt/build/lib/:${GSL}:${BOOST}:
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/build/lib:${PWD}/../../vdt/build/lib/:${GSL}:${BOOST}:${TBB}:
+export LIBRARY_PATH=${LIBRARY_PATH}:${PWD}/build/lib:${PWD}/../../vdt/build/lib/:${GSL}:${BOOST}:${TBB}:
 # export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/build/lib:${PWD}/../../vdt/build/lib/
 export PYTHONPATH=${PYTHONPATH}:${PWD}/build/lib/python/
 export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:../../vdt/:${HOME}/local/eigen-3.4.0/
